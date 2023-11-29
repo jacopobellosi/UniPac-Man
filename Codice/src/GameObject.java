6a7c0243978917c07073bd814d9445b5c260326c
jacopobellosi <j.bellosi@studenti.unibg.it>
2023-11-29 11:49:45 +0100

    Aggiunta classi create con Rebel

---
+++ Codice/src/GameObject.java
@@ -0,0 +1,16 @@
+
+
+import java.util.*;
+import java.time.*;
+
+
+
+// ----------- << imports@AAAAAAGMGoLZ5WctOXA= >>
+// ----------- >>
+
+// ----------- << class.annotations@AAAAAAGMGoLZ5WctOXA= >>
+// ----------- >>
+public class GameObject {
+// ----------- << class.extras@AAAAAAGMGoLZ5WctOXA= >>
+// ----------- >>
+}
