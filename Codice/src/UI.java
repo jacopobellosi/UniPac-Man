1590de3003a4f763e462c956a723bc2eff855f2a
LLonghi02 <lara.longhi3@gmail.com>
2023-12-24 17:35:41 +0100

    Update UI.java

--- Codice/src/UI.java
+++ Codice/src/UI.java
@@ -4,20 +4,24 @@ import java.awt.Color;
 import java.awt.Font;
 public class UI {
 	GameEngine gp;
-	Font arial_40;
-	Font arial_80B;
+	Font f_30;
+	Font f_50;
+	Font c_30;
 
 	public boolean gameFinished=false;
 	
 	public UI(GameEngine gp) {
 		this.gp = gp;
-		arial_40 = new Font("arial", Font.PLAIN,40);
-		arial_80B = new Font("arial", Font.BOLD,40);
+		f_30 = new Font("PacFont Good", Font.PLAIN,30);
+		f_50 = new Font("PacFont Good", Font.BOLD,50);
+		
+		c_30 = new Font("calibri", Font.PLAIN,30);
+
 
 	}
 	public void drawContaPallini(Graphics2D g2) {
 		if(gameFinished==true) {
-			g2.setFont(arial_40);
+			g2.setFont(f_30);
 			g2.setColor(Color.white);
 
 			String text;
@@ -29,26 +33,29 @@ public class UI {
 			textLength=(int)g2.getFontMetrics().getStringBounds(text, g2).getWidth();
 			
 			x=gp.screenWidth/2-textLength/2;
-			y=gp.screenHeight/2-(gp.titleSize*3);
+			y=gp.screenHeight/2-(gp.titleSize*2);
 			g2.drawString(text, x, y);
 			
-			g2.setFont(arial_80B);
+			g2.setFont(f_50);
 			g2.setColor(Color.white);
 			
 			text= "TI SEI LAUREATO!";
 			textLength=(int)g2.getFontMetrics().getStringBounds(text, g2).getWidth();
 			
 			x=gp.screenWidth/2-textLength/2;
-			y=gp.screenHeight/2+(gp.titleSize*2);
+			y=gp.screenHeight/2;//-(gp.titleSize*4);
 			g2.drawString(text, x, y);
 			
 			gp.gameThread=null;
 
 		}else {
-			g2.setFont(arial_40);
+			g2.setFont(f_30);
 			g2.setColor(Color.white);
-			g2.drawString("Pallini = "+gp.player.hashKey, 25, 50);
+			g2.drawString("Pallini = ", 25, 50);
 		
+			g2.setFont(c_30);
+			g2.setColor(Color.white);
+			g2.drawString(""+gp.player.hashKey, 200, 49);
 		}
 		
 	}
