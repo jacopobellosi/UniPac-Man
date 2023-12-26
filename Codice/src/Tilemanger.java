fd4ba77a6363677b6ebe8d6261c96d0179be4a15
LLonghi02 <lara.longhi3@gmail.com>
2023-12-26 22:43:41 +0100

    bordi

--- Codice/src/Tilemanger.java
+++ Codice/src/Tilemanger.java
@@ -15,7 +15,7 @@ public class Tilemanger {
 	static int conteggio;
 	public  Tilemanger(GameEngine gp) {
 		this.gp = gp;
-		tile= new Obstacole[10];
+		tile= new Obstacole[15];
 		mapTilenum = new int[gp.maxScreenCol][gp.maxScreenRow];
 		getTileImage();
 		conteggio=0;
@@ -29,12 +29,58 @@ public class Tilemanger {
 			tile[0].image = ImageIO.read(getClass().getResourceAsStream("/pacman/tile/sfondo.jpg"));
 			
 			tile[1] = new Obstacole();
+<<<<<<< HEAD
 			tile[1].image = ImageIO.read(getClass().getResourceAsStream("/pacman/border/border_verticale.png"));
 			tile[1].collision = true;
 			
 			tile[2] = new Obstacole();
 			tile[2].image = ImageIO.read(getClass().getResourceAsStream("/pacman/border/border_orizzontale.png"));
+=======
+			tile[1].image = ImageIO.read(getClass().getResourceAsStream("/pacman/border/innerborder_verticale.png"));
+			tile[1].collision = true;
+			
+			tile[2] = new Obstacole();
+			tile[2].image = ImageIO.read(getClass().getResourceAsStream("/pacman/border/innerborder_orizzontale.png"));
+>>>>>>> ec4ffc44f5d8dbeea349514ae8c32a29629c71bb
 			tile[2].collision = true;
+			
+			tile[3]=new Obstacole();
+			tile[3].image = ImageIO.read(getClass().getResourceAsStream("/pacman/border/innerborder_angolo_adx.png"));
+			tile[3].collision = true;
+			
+			tile[4]=new Obstacole();
+			tile[4].image = ImageIO.read(getClass().getResourceAsStream("/pacman/border/innerborder_angolo_asx.png"));
+			tile[4].collision = true;
+			
+			tile[5]=new Obstacole();
+			tile[5].image = ImageIO.read(getClass().getResourceAsStream("/pacman/border/innerborder_angolo_bsx.png"));
+			tile[5].collision = true;
+			
+			tile[6]=new Obstacole();
+			tile[6].image = ImageIO.read(getClass().getResourceAsStream("/pacman/border/innerborder_angolo_bdx.png"));
+			tile[6].collision = true;
+			
+			tile[7]=new Obstacole();
+			tile[7].image = ImageIO.read(getClass().getResourceAsStream("/pacman/border/chiuso.png"));
+			tile[7].collision = true;
+			
+			tile[8]=new Obstacole();
+			tile[8].image = ImageIO.read(getClass().getResourceAsStream("/pacman/border/chiuso_giu.png"));
+			tile[8].collision = true;
+			
+			tile[9]=new Obstacole();
+			tile[9].image = ImageIO.read(getClass().getResourceAsStream("/pacman/border/chiuso_su.png"));
+			tile[9].collision = true;
+			
+			tile[10]=new Obstacole();
+			tile[10].image = ImageIO.read(getClass().getResourceAsStream("/pacman/border/chiuso_dx.png"));
+			tile[10].collision = true;
+			
+			tile[11]=new Obstacole();
+			tile[11].image = ImageIO.read(getClass().getResourceAsStream("/pacman/border/chiuso_sx.png"));
+			tile[11].collision = true;
+			
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
