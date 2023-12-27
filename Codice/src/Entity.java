96d5ad368b151bf2a500b676ac017b75ab9776e0
jacopobellosi <79263590+jacopobellosi@users.noreply.github.com>
2023-12-27 14:24:37 +0100

    uccidiamo fantasmi

--- Codice/src/Entity.java
+++ Codice/src/Entity.java
@@ -6,7 +6,7 @@ import java.awt.image.BufferedImage;
 public class Entity {
 		public int x,y;
 	   public int speed;
-	   public Image up1,up2, down1,down2,left,right, imageGhost,logo;
+	   public Image up1,up2, down1,down2,left,right, imageGhost,logo, imageFantasma_vunerabile;
 	   public String direction;
 	   public boolean collision=false;
 	   public Rectangle solidArea = new Rectangle(0,0,48,48);
@@ -15,6 +15,7 @@ public class Entity {
 	   public boolean invincible = false;
 	   public int invincibleCounter=0;
 	   public int actionlockCounter=0;
+	   public int type;
 	   String name;
 	   	public int solidAreaDefaultx, solidAreaDefaulty;
 	   	GameEngine gp;
@@ -28,9 +29,14 @@ public class Entity {
 		   gp.cCheck.checktile(this);
 		   //gp.cCheck.checkObject(this, false);
 		   gp.cCheck.checkEntity(this, gp.ghost);
-		   gp.cCheck.checkPlayer(this);
+		   boolean contactPlayer = gp.cCheck.checkPlayer(this);
 		   
-		   
+		   if(this.type==1 && contactPlayer==true) {
+			   if(gp.player.invincible==false) {
+				   gp.player.life-=1;
+				   gp.player.invincible=true;
+			   }
+		   }
 
 		   if(direction=="up" && collisionON == false) {
 	    		y -=speed;
