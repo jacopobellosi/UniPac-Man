a19428527835e173893557781d32575c8ac39f1b
jacopobellosi <79263590+jacopobellosi@users.noreply.github.com>
2023-12-31 12:08:37 +0100

    X lara

---
+++ Codice/src/demo/prova.java
@@ -0,0 +1,14 @@
+package demo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class prova {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
