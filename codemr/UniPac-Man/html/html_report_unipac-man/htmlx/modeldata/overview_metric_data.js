dce09c2e4a6428deca904efb72344790930afcbd
PoloniLuca <l.poloni4@studenti.unibg.it>
2024-01-16 15:26:21 +0100

    Architettura software e inizio design pattern con implementazione di CodeMR

---
+++ codemr/UniPac-Man/html/html_report_unipac-man/htmlx/modeldata/overview_metric_data.js
@@ -0,0 +1,1 @@
+var CodeMR_ProjectMetrics = {"Project Name":"UniPac-Man working set: {UniPac-Man}", "Number of Highly Problematic Classes":0, "Number of Entities":19, "Number of Problematic Classes":0, "Class Lines of Code":1218, "Number of External Packages":11, "Number of Packages":7, "Number of External Entities":39}
