2eeafd41d9358a5e71b26483c5df43b60a18a2a6
PoloniLuca <l.poloni4@studenti.unibg.it>
2024-02-20 10:46:46 +0100

    Creazione dipendenze maven

---
+++ Code/Codice/Model/src/main/java/UniPacMan/Model/App.java
@@ -0,0 +1,13 @@
+package UniPacMan.Model;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Code/Codice/View/src/main/java/UniPacMan/View/App.java
@@ -0,0 +1,13 @@
+package UniPacMan.View;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
