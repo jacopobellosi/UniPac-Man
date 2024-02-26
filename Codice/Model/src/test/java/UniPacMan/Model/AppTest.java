65d6c5ee9b6032a5fc2884077f01e873ef4fe585
PoloniLuca <l.poloni4@studenti.unibg.it>
2024-02-26 22:03:54 +0100

    test model e view

--- Codice/Model/src/test/java/UniPacMan/Model/AppTest.java
+++ Codice/Model/src/test/java/UniPacMan/Model/AppTest.java
@@ -1,20 +1,24 @@
 package UniPacMan.Model;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+
+import model.AssetSetter;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+	@Test
+    public void testSpawnPointGhost() {
+        AssetSetter assetSetter = new AssetSetter();
+        int i = 2;
+        int j = 1;
+        int expected = 4;
+        int actual = assetSetter.spawnPointGhost(i, j);
+        assertEquals(expected, actual);
     }
 }
--- Codice/View/src/test/java/UniPacMan/View/AppTest.java
+++ Codice/View/src/test/java/UniPacMan/View/AppTest.java
@@ -1,20 +1,28 @@
 package UniPacMan.View;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+
+import view.Sound;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+	@Test
+    public void testPlayWin() {
+        Sound sound = new Sound();
+        sound.setFile(0); 
+        assertFalse(sound.isPlaying()); //verifico che il suono non sia già in esecuzione
+       
+        sound.playWin();
+        assertTrue(sound.isPlaying());
+        /* Se si stoppa il suono e si verifica se continua a suonare si avrà un test fallito
+        sound.stop();
+        assertFalse(sound.isPlaying());
+    	*/
     }
 }
